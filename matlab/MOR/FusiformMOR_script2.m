7a82d9fbe499ff2c2616a2e5b5505694820b119c
Mylena Mordhorst <mordhorst@leadx06.(none)>
2014-08-14 16:47:22 +0200

    new setup with logspace for parameters

--- matlab/MOR/FusiformMOR_script2.m
+++ matlab/MOR/FusiformMOR_script2.m
@@ -8,25 +8,39 @@ clear all;
 close all;
 clear classes;
 clc;
+%
+%%
 % build model
 modconf = FusiformMORexample;
 geo = modconf.PosFE.Geometry;
 model = muscle.Model(modconf);
 %
-% vary tolerances
 %%
-model.ODESolver.RelTol = 0.1;
-model.ODESolver.AbsTol = 0.4;
+% vary tolerances
+model.ODESolver.RelTol = 0.5;
+model.ODESolver.AbsTol = 0.5;
+%
 %%
 % create parameter grid
-% 
 s = sampling.GridSampler;
 model.Sampler = s;
-
+%
+model.Data.TrajectoryData.UniformTrajectories = false;
+model.Data.TrajectoryFxiData.UniformTrajectories = false;
+%
 model.off1_createParamSamples;
-%model.Data.ParamSamples = model.Data.ParamSamples(:,1:3);
 model.TrainingInputs = 1;
 model.off2_genTrainingData;
 model.save;
+%
+%%
+% do offline phase 3 
+%
+% first simple
+%model.SpaceReducer.MaxSubspaceSize = 1000;
+%model.off3_computeReducedSpace;
+%model.save;
+%
+% then test including fxi data
+%IncludeTrajectoryFxiData = true;
 
-%save(fullfile(FusiformMORexample.OutputDir,'trajectoryData'),'model');
