0704f286ba785bfebb22a7abeee34ac3208e61b2
Mylena Mordhorst <mylena.mordhorst@mechbau.uni-stuttgart.de>
2014-09-12 10:28:00 +0200

    update video output of LongMOR parameter space plotting

--- matlab/MOR/LongMOR_script_plotTrajectories.m
+++ matlab/MOR/LongMOR_script_plotTrajectories.m
@@ -76,7 +76,7 @@ h = pm.nextPlot('zpos_visc0p5_act-normalised','Visualisation of  precomputed tra
             'activation in 24ms','activation in 28ms','activation in 34ms',...
             'activation in 41ms','activation in 49ms','activation in 58ms',...
             'activation in 69ms','activation in 83ms','activation in 100ms','Location','SouthEast'});
-sel = 1:10:100;% visc 0.5 - 1, visc 5.1 - 8; visc 10 - 10
+sel = 1:10:100;% start with: visc 0.5 - 1, visc 5.1 - 8; visc 10 - 10
 act_times = m.Data.ParamSamples(2,sel);
 cc = hsv(10);
 for i = 1:10
@@ -89,33 +89,6 @@ hold off;
 pm.done;
 %
 %%
-pm = PlotManager;
-pm.LeaveOpen = true;
-pm.FilePrefix = 'pos_of_precomp_traj';
-%
-% plot over parameter space
-%
-paramspace_visc = reshape(m.Data.ParamSamples(1,:)',10,10);
-paramspace_act = reshape(m.Data.ParamSamples(2,:)',10,10);
-%
-h = pm.nextPlot('ypos','Visualisation of  precomputed trajectories',...
-    'time to full activation','viscosity');
-zlabel(h,'z-position of middle node');
-axis(h,[20 100 0.5 10 0 8]);
-hold(h,'on');
-%
-for i = 1:101
-    mesh_oz = reshape(oz(:,i),10,10);
-    cla(h);
-    %
-    mesh(h,paramspace_act,paramspace_visc,mesh_oz);
-    pause(.1);%drawnow;
-end
-%
-pm.done;
-%
-%
-%%
 % plot or video over parameter space
 video = false;
 %
@@ -126,11 +99,11 @@ pm.FilePrefix = 'pos_of_precomp_traj';
 if video
     avifile = fullfile(LongMORexample.OutputDir,'parameterspace.avi');
     vw = VideoWriter(avifile);
-    vw.FrameRate = 25;
+    vw.FrameRate = 10;
     vw.open;
 end
 %
-h = pm.nextPlot('zpos',sprintf('z-position of middle node at t=%g',t(end)),...
+h = pm.nextPlot('mesh_zpos',sprintf('z-position of middle node at t=%g',t(end)),...
     'time to full activation','viscosity');
 zlabel(h,'z-position of middle node');
 axis(h,[20 100 0.5 10 0 8]);
@@ -143,8 +116,10 @@ for ts = 1:length(t)
     mesh_oz = reshape(oz(:,ts),10,10);
     cla(h);
     %
-    mesh(h,paramspace_act,paramspace_visc,mesh_oz);
+    surfc(h,paramspace_act,paramspace_visc,mesh_oz,'FaceColor','interp','EdgeColor','interp');
     title(h,sprintf('z-position of middle node at t=%g',t(ts)));
+    caxis([0,8]);
+    %colorbar;
     %
     if video
         vw.writeVideo(getframe(gcf));
@@ -160,7 +135,8 @@ end
 pm.done;
 %
 %%
-% save
+% saving
+%
 pm.SaveFormats = {'jpg','eps'};
 pm.ExportDPI = 300;
 pm.UseFileTypeFolders = false;
